49749cac5e6c88d0ba27f17e167152d6b637c615
Rajesh Madamanchi <rmadamanchi@gmail.com>
2025-03-01 08:33:04 -0600

    2024

--- src/2024/index.js
+++ src/2024/index.js
@@ -97,9 +97,9 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     }
 
     renderBarChart('days', [
-        {label: 'Saturday', value: 26 },
-        {label: 'Sunday', value: 24 },
-        {label: 'Thursday', value: 3 },
+        {label: 'Saturday', value: 37 },
+        {label: 'Sunday', value: 33 },
+        {label: 'Thursday', value: 4 },
         {label: 'Friday', value: 2 },
         {label: 'Monday', value: 1 },
         {label: 'Tuesday', value: 1 },
@@ -110,10 +110,13 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 
     renderBarChart('months', [
         {label: 'August', value: 11 },
+        {label: 'October', value: 9 },
         {label: 'September', value: 9 },
-        {label: 'March', value: 8 },
         {label: 'June', value: 8 },
         {label: 'July', value: 8 },
+        {label: 'December', value: 8 },
+        {label: 'March', value: 8 },
+        {label: 'November', value: 4 },
         {label: 'May', value: 4 },
         {label: 'February', value: 4 },
         {label: 'January', value: 3 },
@@ -127,68 +130,69 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     ]);
 
     renderBarChart('flights_by_day', [
-        {label: 'Sat, Sep 14', value: 22 },
+        {label: 'Sat, Nov 9', value: 23 },
         {label: 'Sat, Feb 24', value: 22 },
         {label: 'Sun, Jun 23', value: 22 },
+        {label: 'Sat, Sep 14', value: 22 },
         {label: 'Sat, Mar 2', value: 20 },
-        {label: 'Sat, Sep 7', value: 20 },
         {label: 'Sat, Mar 23', value: 20 },
-        {label: 'Sat, Aug 10', value: 19 },
+        {label: 'Sat, Oct 5', value: 20 },
+        {label: 'Sat, Sep 7', value: 20 },
         {label: 'Sat, Jun 22', value: 19 },
+        {label: 'Sat, Aug 10', value: 19 },
         {label: 'Sat, Aug 3', value: 19 },
-        {label: 'Sat, Aug 24', value: 19 },
-        {label: 'Sat, Sep 21', value: 18 },
+
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
     })
 
     renderBarChart('tows', [
-        {label: 'Ron Wallis', value: 96 },
-        {label: 'Doug Witkowski', value: 91 },
-        {label: 'Pat Gibbons', value: 82 },
-        {label: 'Greg Aaron', value: 69 },
-        {label: 'Marc Augustin', value: 54 },
-        {label: 'Peter Jarvis', value: 44 },
-        {label: 'Lawrence Spinetta', value: 42 },
+        {label: 'Ron Wallis', value: 121 },
+        {label: 'Doug Witkowski', value: 103 },
+        {label: 'Pat Gibbons', value: 89 },
+        {label: 'Greg Aaron', value: 81 },
+        {label: 'Peter Jarvis', value: 75 },
+        {label: 'Marc Augustin', value: 60 },
+        {label: 'Lawrence Spinetta', value: 48 },
+        {label: 'Schaefer Richard', value: 45 },
         {label: 'Tom Barkow', value: 40 },
         {label: 'Smokey Doll', value: 35 },
-        {label: 'Schaefer Richard', value: 23 },
-        {label: 'Augustine Marc', value: 15 },
+        {label: 'Augustine Marc', value: 26 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
     })
 
     renderBarChart('flights_by_pilot', [
-        {label: 'Greg Aaron', value: 80 },
-        {label: 'Bill Brinkman', value: 57 },
-        {label: 'Lawrence Spinetta', value: 51 },
-        {label: 'Justin Knaplund', value: 33 },
-        {label: 'Ingo Kuenzel', value: 32 },
-        {label: 'Peter English', value: 28 },
-        {label: 'Zimri Leisher', value: 27 },
-        {label: 'Ely Bins', value: 26 },
-        {label: 'Ajeeth Rajan', value: 24 },
-        {label: 'Josh Hagler', value: 23 },
-        {label: 'Luis Mailly', value: 22 },
+        {label: 'Greg Aaron', value: 100 },
+        {label: 'Lawrence Spinetta', value: 76 },
+        {label: 'Bill Brinkman', value: 72 },
+        {label: 'Justin Knaplund', value: 40 },
+        {label: 'Peter English', value: 37 },
+        {label: 'Ajeeth Rajan', value: 36 },
+        {label: 'Ely Bins', value: 34 },
+        {label: 'Ingo Kuenzel', value: 33 },
+        {label: 'Zimri Leisher', value: 31 },
+        {label: 'Kip Harris', value: 26 },
+        {label: 'Luis Mailly', value: 26 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
     })
 
     renderBarChart('solo_flights_by_pilot', [
-        {label: 'Ely Bins', value: 25 },
-        {label: 'Luis Mailly', value: 22 },
-        {label: 'Andre DeBaghy', value: 21 },
-        {label: 'Lawrence Spinetta', value: 18 },
+        {label: 'Ely Bins', value: 33 },
+        {label: 'Luis Mailly', value: 26 },
+        {label: 'Andre DeBaghy', value: 24 },
+        {label: 'Lawrence Spinetta', value: 24 },
+        {label: 'Greg Aaron', value: 22 },
         {label: 'Rajesh Madamanchi', value: 17 },
-        {label: 'Greg Aaron', value: 14 },
-        {label: 'Ed Khan', value: 13 },
-        {label: 'Zimri Leisher', value: 13 },
-        {label: 'Ron Wallis', value: 11 },
-        {label: 'Doug Witkowski', value: 11 },
-        {label: 'Scott Marshall', value: 10 },
+        {label: 'Ed Khan', value: 17 },
+        {label: 'Ajeeth Rajan', value: 16 },
+        {label: 'Kip Harris', value: 16 },
+        {label: 'Zimri Leisher', value: 15 },
+        {label: 'Jon Henderson', value: 13 },
 
     ], {
         indexAxis: 'y',
@@ -196,31 +200,30 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     })
 
     renderBarChart('dual_flights_by_pilot', [
-        {label: 'Greg Aaron', value: 66 },
-        {label: 'Bill Brinkman', value: 53 },
-        {label: 'Lawrence Spinetta', value: 33 },
-        {label: 'Justin Knaplund', value: 30 },
+        {label: 'Greg Aaron', value: 78 },
+        {label: 'Bill Brinkman', value: 67 },
+        {label: 'Lawrence Spinetta', value: 52 },
+        {label: 'Justin Knaplund', value: 33 },
+        {label: 'Peter English', value: 27 },
         {label: 'Ingo Kuenzel', value: 26 },
-        {label: 'Josh Hagler', value: 21 },
-        {label: 'Peter English', value: 21 },
-        {label: 'Zimri Leisher', value: 14 },
-        {label: 'Ajeeth Rajan', value: 14 },
-        {label: 'Atharva Sehgal', value: 10 },
-        {label: 'Kip Harris', value: 10 },
+        {label: 'Josh Hagler', value: 23 },
+        {label: 'Ajeeth Rajan', value: 20 },
+        {label: 'Zimri Leisher', value: 16 },
+        {label: 'David Learned', value: 15 },
+        {label: 'Sanjay Shreedharan', value: 13 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
     })
 
     renderBarChart('demo_flights', [
-        {label: 'Justin Knaplund', value: 27 },
-        {label: 'Peter English', value: 16 },
+        {label: 'Justin Knaplund', value: 30 },
+        {label: 'Peter English', value: 22 },
+        {label: 'Lawrence Spinetta', value: 13 },
         {label: 'Greg Aaron', value: 9 },
-        {label: 'Lawrence Spinetta', value: 8 },
+        {label: 'Bill Brinkman', value: 7 },
         {label: 'Ingo Kuenzel', value: 3 },
         {label: 'Mitch Lubars', value: 3 },
-        {label: 'Bill Brinkman', value: 1 },
-
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
@@ -230,15 +233,15 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     renderBarChart('tows_per_day', [
         {label: 'Doug Witkowski (Sun, Jun 23)', value: 20 },
         {label: 'Marc Augustin (Sat, Aug 10)', value: 19 },
-        {label: 'Doug Witkowski (Sat, Aug 3)', value: 19 },
         {label: 'Pat Gibbons (Sat, Jun 22)', value: 19 },
+        {label: 'Doug Witkowski (Sat, Aug 3)', value: 19 },
+        {label: 'Ron Wallis (Sat, Nov 9)', value: 17 },
         {label: 'Greg Aaron (Sun, Jun 9)', value: 17 },
+        {label: 'Peter Jarvis (Sun, Oct 27)', value: 15 },
         {label: 'Ron Wallis (Sat, Jun 15)', value: 15 },
+        {label: 'Schaefer Richard (Sat, Oct 5)', value: 15 },
         {label: 'Peter Jarvis (Sat, Jul 20)', value: 15 },
-        {label: 'Greg Aaron (Sat, Sep 21)', value: 14 },
-        {label: 'Tom Barkow (Sat, Mar 2)', value: 14 },
         {label: 'Pat Gibbons (Sat, Mar 23)', value: 14 },
-        {label: 'Ron Wallis (Sun, Sep 22)', value: 14 },
 
     ], {
         indexAxis: 'y',
@@ -247,16 +250,16 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 
     renderBarChart('flights_per_day', [
         {label: 'Bill Brinkman (Sat, Aug 31)', value: 8 },
-        {label: 'Greg Aaron (Sat, Jun 15)', value: 8 },
         {label: 'Justin Knaplund (Sat, Sep 14)', value: 8 },
         {label: 'Bill Brinkman (Sat, Aug 24)', value: 8 },
+        {label: 'Greg Aaron (Sat, Jun 15)', value: 8 },
         {label: 'Peter English (Sat, Sep 7)', value: 7 },
         {label: 'Bill Brinkman (Sun, May 19)', value: 7 },
+        {label: 'Lawrence Spinetta (Sun, Oct 20)', value: 7 },
+        {label: 'Lawrence Spinetta (Sat, Nov 9)', value: 7 },
         {label: 'Greg Aaron (Sat, Jul 20)', value: 7 },
-        {label: 'Justin Knaplund (Sat, Sep 7)', value: 6 },
-        {label: 'Bill Brinkman (Sat, Sep 14)', value: 6 },
-        {label: 'Ingo Kuenzel (Sun, Jul 14)', value: 6 },
         {label: 'Greg Aaron (Sun, May 19)', value: 6 },
+        {label: 'Greg Aaron (Sat, Nov 9)', value: 6 },
 
     ], {
         indexAxis: 'y',
@@ -264,35 +267,34 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     })
 
     renderBarChart('active_days', [
-        {label: 'Greg Aaron', value: 31 },
-        {label: 'Lawrence Spinetta', value: 28 },
-        {label: 'Ely Bins', value: 24 },
-        {label: 'Andre DeBaghy', value: 20 },
-        {label: 'Ron Wallis', value: 20 },
-        {label: 'Luis Mailly', value: 19 },
-        {label: 'Bill Brinkman', value: 14 },
-        {label: 'Doug Witkowski', value: 14 },
-        {label: 'Ajeeth Rajan', value: 14 },
-        {label: 'Kip Harris', value: 13 },
-        {label: 'Josh Hagler', value: 12 },
-
+        {label: 'Greg Aaron', value: 40 },
+        {label: 'Lawrence Spinetta', value: 35 },
+        {label: 'Ely Bins', value: 31 },
+        {label: 'Ron Wallis', value: 24 },
+        {label: 'Luis Mailly', value: 23 },
+        {label: 'Andre DeBaghy', value: 23 },
+        {label: 'Ajeeth Rajan', value: 21 },
+        {label: 'Bill Brinkman', value: 19 },
+        {label: 'Kip Harris', value: 17 },
+        {label: 'Doug Witkowski', value: 16 },
+        {label: 'Ed Khan', value: 16 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25
     })
 
     renderBarChart('longest_flights', [
+        {label: 'Ely Bins (Sun, Sep 15)', value: 357 },
+        {label: 'Ely Bins (Thu, Oct 3)', value: 356 },
+        {label: 'Ely Bins (Sun, Oct 6)', value: 332 },
         {label: 'Lawrence Spinetta (Sat, Jul 20)', value: 330 },
-        {label: 'Ely Bins (Sat, Jul 20)', value: 325 },
+        {label: 'Ely Bins (Sat, Jul 20)', value: 329 },
         {label: 'Ely Bins (Tue, Aug 13)', value: 319 },
         {label: 'Jason Greene (Thu, Sep 19)', value: 311 },
+        {label: 'Ely Bins (Sat, Oct 5)', value: 308 },
         {label: 'Ely Bins (Sun, Jun 30)', value: 305 },
         {label: 'Ely Bins (Sat, Sep 21)', value: 303 },
         {label: 'Ely Bins (Thu, Jul 11)', value: 293 },
-        {label: 'Ely Bins (Sun, Aug 18)', value: 289 },
-        {label: 'Ely Bins (Sat, Jun 22)', value: 288 },
-        {label: 'Lawrence Spinetta (Sun, Jun 30)', value: 286 },
-        {label: 'Ely Bins (Sun, Sep 22)', value: 283 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25,
@@ -300,17 +302,17 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
     })
 
     renderBarChart('total_flight_time', [
-        {label: 'Ely Bins', value: 5321 },
-        {label: 'Lawrence Spinetta', value: 3682 },
-        {label: 'Andre DeBaghy', value: 2081 },
-        {label: 'Greg Aaron', value: 2075 },
-        {label: 'Luis Mailly', value: 1429 },
-        {label: 'Kip Harris', value: 1086 },
-        {label: 'Bill Brinkman', value: 953 },
-        {label: 'Ingo Kuenzel', value: 872 },
-        {label: 'Rajesh Madamanchi', value: 851 },
-        {label: 'Ed Khan', value: 812 },
-        {label: 'Jason Greene', value: 735 },
+        {label: 'Ely Bins', value: 6978 },
+        {label: 'Lawrence Spinetta', value: 4534 },
+        {label: 'Greg Aaron', value: 2561 },
+        {label: 'Andre DeBaghy', value: 2519 },
+        {label: 'Luis Mailly', value: 1693 },
+        {label: 'Ed Khan', value: 1412 },
+        {label: 'Kip Harris', value: 1340 },
+        {label: 'Bill Brinkman', value: 1169 },
+        {label: 'Ingo Kuenzel', value: 1012 },
+        {label: 'Ajeeth Rajan', value: 993 },
+        {label: 'Peter English', value: 858 },
     ], {
         indexAxis: 'y',
         yLabelFontSize: 25,
